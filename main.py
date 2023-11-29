5b7d167252f3f99e4e6d9c0e53ea01c7cd9e251e
Marina Kirillova <marinkamalysheva@mail.ru>
2023-11-29 17:31:57 +0300

    change 5

--- main.py
+++ main.py
@@ -7,27 +7,24 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
-
     def rate_lec(self, lecturer, course, grade):
-        if isinstance(lecturer, Mentor) and course in self.courses_attached and course in lecturer.courses_in_progress:
+        marks = [0,1,2,3,4,5,6,7,8,9,10]
+        if isinstance(lecturer, Mentor) and grade in marks:
             if course in lecturer.grades:
                 lecturer.grades[course] += [grade]
             else:
                 lecturer.grades[course] = [grade]
         else:
             return 'Ошибка'
-
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached =[]
-
 class Lecturer(Mentor):
     def __init__(self, name, surname, grades):
         super().__init__(name, surname)
         self.grades ={}
-
     def av_gr(self):
         sum_gr = 0
         len_gr = 0
@@ -35,12 +32,8 @@ class Lecturer(Mentor):
             sum_gr += sum(course)
             len_gr += len(course)
             average_grade = round(sum_gr / len_gr)
-
-
     def __str__(self):
         return f"Имя: {self.name} \nФамилия: {self.surname} \nСредняя оценка за лекции: {self.av_gr()}"
-
-
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -50,21 +43,17 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
-
     def __str__(self):
         return f"Имя: {self.name} \nФамилия: {self.surname}"
-
-
-
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
-
 cool_lecturer = Lecturer('Some', 'Buddy', grades=10)
 cool_lecturer.courses_attached += ['Python']
+
+lecturer1 = Lecturer ('Some', 'Buddy')
+student1.rate_lec(lecturer1, 'Sql', 9)
 some_reviewer = Reviewer('Some', 'Buddy')
 some_lecturer = Lecturer('Some', 'Buddy', grades=())
-
-
 
 print(some_reviewer)
 print(some_lecturer)
